ebee124dd1eb70fc4d2a2e1620ee18f71dc298d9
BaxtiyorXusanov <xusanovbaxtiyor07>
2021-09-12 20:57:50 +0500

    changed mistake

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 const elNumber = document.querySelector('#number');
-// const elBtn_reset = document.querySelector('#btn-reset');
-// const elBtn_add = document.querySelector('#btn-add');
-const elBtns = document.querySelectorAll('.btn')
+const elBtn_reset = document.querySelector('#btn-reset');
+const elBtn_add = document.querySelector('#btn-add');
+// const elBtns = document.querySelectorAll('.btn')
 
 let count = 0;
 
-elBtns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
-        const styles = e.currentTarget.classList;
-        if(styles.contains('btn-add')){
-            count++;
-        }
-        else if(styles.contains('btn-reset')){
-            count = 0
-        }
-        // if(count > 0){
-        //     value.style.color = "green";
-        // }
-        // if(count < 0){
-        //     value.style.color = "red"
-        // }
-        // if(count === 0){
-        //     value.style.color = "#222"
-        // }
-        elNumber.textContent = count;
-    })
-    // console.log(btn)
+elBtn_add.addEventListener("click", function (e) {
+    // e.preventDefault();
+    count++
+    elNumber.textContent = count;
 })
+elBtn_reset.addEventListener("click", function (e) {
+    // e.preventDefault();
+    count = 0;
+    elNumber.textContent = count;
+})
+console.log(count)
